8017b7a4b3709997df09199d9ce899ce2756bd2a
Randy Williams <randy.williams@allscripts.com>
2016-05-11 11:00:30 -0400

    Initial Commit

---
+++ Alchemy Lab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Alchemy Lab
+//
+//  Created by Randy Williams on 5/11/16.
+//  Copyright © 2016 alchemy Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Alchemy Lab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Alchemy Lab
+//
+//  Created by Randy Williams on 5/11/16.
+//  Copyright © 2016 alchemy Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
